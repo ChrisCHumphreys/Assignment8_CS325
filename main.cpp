ee5a757065fb2a1c78f7585bfa4fa093ef6b6eb9
Chris Humphreys <geochris35>
2018-04-05 08:00:36 -0500

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * Name: Chris Humphreys
+ * Date: 04/04/18
+ * File: main.cpp
+ * Description:  Main driver for Assignment 8 binary search tree assignment.  Creates a user menu, 
+ *   and runs program in an easy to use interface.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello World" << endl;
+  return 0;
+}
+
